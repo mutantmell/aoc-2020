969a793178140d920ca9c66bac79f8178d346ad3
Matt Mullen <malaguy@gmail.com>
2020-12-04 20:41:08 -0800

    a version with Either instead of Maybe.
    Could be better about returning good error messages

--- lib/Day4.hs
+++ lib/Day4.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE RecordWildCards #-}
@@ -7,6 +9,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 
 module Day4 where
 
@@ -18,8 +21,8 @@ import qualified Data.Char as Char
 import Data.Text (Text)
 import qualified Data.Map as Map
 import qualified Data.Text.IO as Text
-import Control.Monad ((<=<))
-import Control.Lens ((^?), ix, filtered, to, _Right)
+import Control.Monad ((<=<), foldM)
+import Control.Lens (IxValue, Getter, Index, At, Lens', at, (^.), (^?), ix, filtered, to, _Right)
 import Control.Applicative (Applicative(liftA2), (<|>))
 import Data.Functor (($>))
 import qualified Data.Text as Text
@@ -75,7 +78,18 @@ parseHair = (<>) <$> Atto.string "#" <*> (Text.pack <$> Atto.count 6 (Atto.satis
 parsePid :: Atto.Parser Text
 parsePid = Text.pack <$> Atto.count 9 (Atto.satisfy Char.isDigit) -- this read is safe
 
-fromMap :: Map.Map Text Text -> Maybe Passport -- make this an either?
+asError :: e -> Getter (Maybe a) (Either e a)
+asError e = to $ maybe (Left e) Right
+
+at' :: (At m, Show (Index m)) => Index m -> Getter m (Either String (IxValue m))
+at' key = at key . asError ("unknown key: " <> show key)
+
+validated :: (Foldable t, Show a) => t (a -> Bool) -> a -> Either String a
+validated vs a = foldM f a vs
+  where
+    f a v = if v a then Right a else Left ("invalid value: " <> show a)
+
+fromMap :: Map.Map Text Text -> Maybe Passport
 fromMap map = do
   birthYear <- map ^? ix "byr" . to (parseAll parseYear) . _Right . filtered ((>= 1920) <&&> (<= 2002))
   issueYear <- map ^? ix "iyr" .  to (parseAll parseYear) . _Right . filtered ((>= 2010) <&&> (<= 2020))
@@ -89,5 +103,26 @@ fromMap map = do
   let countryId = map ^? ix "cid"
   pure Passport{..}
 
+implies :: (a -> Bool) -> (a -> Bool) -> a -> Bool
+a `implies` b = \x -> not (a x) || b x
+
+fromMap' :: Map.Map Text Text -> Either String Passport
+fromMap' map = do
+  birthYear <- map ^. at' "byr" >>= parseAll parseYear >>= validated [(>= 1920), (<= 2002)]
+  issueYear <- map ^. at' "iyr" >>= parseAll parseYear >>= validated [(>= 2010), (<= 2020)]
+  expirationYear <- map ^. at' "eyr" >>= parseAll parseYear >>= validated [(>= 2020), (<= 2030)]
+  height <- map ^. at' "hgt" >>= parseAll parseHeight >>= validated
+    [ ((== CM) . snd) `implies` (((150 <=) <&&> (<= 193)) . fst)
+    , ((== In) . snd) `implies` (((59 <=) <&&> (<= 76)) . fst)
+    ]
+  hairColor <- map ^. at' "hcl" >>= parseAll parseHair
+  eyeColor <- map ^. at' "ecl" >>= validated [(`elem` ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"])]
+  passportId <- map ^. at' "pid" >>= parseAll parsePid
+  let countryId = map ^? ix "cid"
+  pure Passport{..}
+
 partB :: [Map.Map Text Text] -> Int
 partB = length . W.mapMaybe fromMap
+
+partB' :: [Map.Map Text Text] -> Int
+partB' = length . W.mapMaybe ((^? _Right) . fromMap')
